08150c93f19583ed82940fc589fc69df526041e5
ryuJin25 <kathareaniket47@gmail.com>
2022-06-09 19:58:18 +0530

    Fix AntiCrash Loop Bug

--- Handlers/AntiCrash.js
+++ Handlers/AntiCrash.js
@@ -9,37 +9,27 @@ const chalk = require("chalk");
  */
 module.exports = async (client) => {
 
-    process.on('multipleResolves', (type, promise, reason) => {
-        console.log(chalk.red(`[MULTIPLE RESOLVES] `));
-        console.log(type, promise, reason)
-        client.user.send("Something went Wrong, Please try again :(")
-    })
-
     process.on('unhandledRejection', (reason, promise) => {
         console.log(chalk.red(`[UNHANDLED REJECTION] `));
         console.log(reason, promise)
-        // process.exit(1) //Comment this out later!!
-        client.user.send("Something went Wrong, Please try again :(")
+    })
+
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log(chalk.red(`[MULTIPLE RESOLVES] `));
+        console.log(type, promise, reason)
     })
 
     process.on('uncaughtException', (err, origin) => {
-        console.log(chalk.red(`[UNCAUGHT EXCEPTION ] `));
+        console.log(chalk.red(`[UNCAUGHT EXCEPTION] `));
         console.log(err, origin)
-        client.user.send("Something went Wrong, Please try again :(")
     })
 
     process.on('uncaughtExceptionMonitor', (err, origin) => {
         console.log(chalk.red(`[UNCAUGHT EXCEPTION MONITOR] `));
         console.log(err, origin)
-        client.user.send("Something went Wrong, Please try again :(")
     })
 
     process.on('warning', (warning) => {
         console.log(chalk.red(`[WARNING] `) + chalk.yellow(`${warning}`))
-        client.user.send("Something went Wrong, Please try again :(")
     })
-
-    // process.on('message', (message) => {
-    //     console.log(`[MESSAGE] `.bold.red + `${message}`.yellow);
-    // })
 }
